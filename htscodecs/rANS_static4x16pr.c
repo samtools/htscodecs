70aedc1ee36ce9161e93d2d8b5ed42d08fdef35a
James Bonfield <jkb@sanger.ac.uk>
2021-05-21 18:00:02 +0100

    Improve compute_shift estimates.
    The calculations are now done as doubles instead of going via int,
    which was skewing the entropy calculation a bit.  Also measured the
    frequency symbol table from some data to get real values.
    
    The net effect on this changes appears to be to use 10-bit freqs more
    often than before, so up to 1% growth for some marginal data series,
    but considerably quicker on them too.

--- htscodecs/rANS_static4x16pr.c
+++ htscodecs/rANS_static4x16pr.c
@@ -650,20 +650,19 @@ static int compute_shift(uint32_t *F0, uint32_t (*F)[256], uint32_t *T,
 
 	double l10 = log(TOTFREQ_O1_FAST + sm10);
 	double l12 = log(TOTFREQ_O1      + sm12);
+	double T_slow = (double)TOTFREQ_O1/T[i];
+	double T_fast = (double)TOTFREQ_O1_FAST/T[i];
 
 	for (j = 0; j < 256; j++) {
 	    if (F[i][j]) {
 		ns++;
 
-		int x = (double)TOTFREQ_O1_FAST * F[i][j]/T[i];
-		e10 -= F[i][j] * (fast_log(MAX(x,1)) - l10);
+		e10 -= F[i][j] * (fast_log(MAX(F[i][j]*T_fast,1)) - l10);
+		e12 -= F[i][j] * (fast_log(MAX(F[i][j]*T_slow,1)) - l12);
 
-		x = (double)TOTFREQ_O1 * F[i][j]/T[i];
-		e12 -= F[i][j] * (fast_log(MAX(x,1)) - l12);
-
-		// Estimation of compressedf symbol freq table too.
-		e10 += 4;
-		e12 += 6;
+		// Estimation of compressed symbol freq table too.
+		e10 += 1.3;
+		e12 += 4.7;
 	    }
 	}
 
@@ -682,7 +681,9 @@ static int compute_shift(uint32_t *F0, uint32_t (*F)[256], uint32_t *T,
 	if (max_tot < max_val)
 	    max_tot = max_val;
     }
-    int shift = e10/e12 < 1.01 || max_tot <= TOTFREQ_O1_FAST ? TF_SHIFT_O1_FAST : TF_SHIFT_O1;
+    int shift = e10/e12 < 1.01 || max_tot <= TOTFREQ_O1_FAST
+	? TF_SHIFT_O1_FAST
+	: TF_SHIFT_O1;
 
 //    fprintf(stderr, "e10/12 = %f %f %f, shift %d\n",
 //    	    e10/log(256), e12/log(256), e10/e12, shift);
