samtools/htscodecs

commit c8dd53c3077bf85b92a1eb9934f9e2f3826e8b75

Ensure CPU dispatch only references functions that exist
If, for example, HAVE_AVX512 is not defined, the various *_avx512
functions won't exist and cannot be referenced.  Rework the
CPU dispatch code to check the HAVE_ macros so it won't try
to use any undefined symbols.  The new version will choose
the best function that both exists, and has CPU support.

Also fixes broken indentation on 'if (do_simd) { ... }' by inverting
the check and handling the false case at the top.

htscodecs/rANS_static4x16pr.c
@@ -836,7 +836,11 @@ unsigned char *(*rans_enc_func(int do_simd, int order))
      unsigned int in_size,
      unsigned char *out,
      unsigned int *out_size) {
-    if (do_simd) {
+    if (!do_simd) { // SIMD disabled
+        return order & 1
+            ? rans_compress_O1_4x16
+            : rans_compress_O0_4x16;
+    }
 	unsigned int eax = 0, ebx = 0, ecx = 0, edx = 0;
 	int have_ssse3   = 0;
 	int have_sse4_1  = 0;
@@ -875,28 +879,34 @@ unsigned char *(*rans_enc_func(int do_simd, int order))
 	if (!(rans_cpu & RANS_CPU_ENC_SSE4))   have_sse4_1 = 0;
 
 	if (order & 1) {
-	    return have_avx512f
-		? rans_compress_O1_32x16_avx512
-		: (have_avx2
-		   ? rans_compress_O1_32x16_avx2
-		   : (have_sse4_1
-		      ? rans_compress_O1_32x16
-		      : rans_compress_O1_32x16));
+#if defined(HAVE_AVX512)
+        if (have_avx512f)
+            return rans_compress_O1_32x16_avx512;
+#endif
+#if defined(HAVE_AVX2)
+        if (have_avx2)
+            return rans_compress_O1_32x16_avx2;
+#endif
+#if defined(HAVE_SSE4_1) && defined(HAVE_SSSE3)
+        if (have_sse4_1) 
+            return rans_compress_O1_32x16;
+#endif
+        return rans_compress_O1_32x16;
 	} else {
-	    return have_avx512f
-		? rans_compress_O0_32x16_avx512
-		: (have_avx2
-		   ? rans_compress_O0_32x16_avx2
-		   : (have_sse4_1
-		      ? rans_compress_O0_32x16//_sse4
-		      : rans_compress_O0_32x16));
-	}
+#if defined(HAVE_AVX512)
+	    if (have_avx512f)
+            return rans_compress_O0_32x16_avx512;
+#endif
+#if defined(HAVE_AVX2)
+		if (have_avx2)
+            return rans_compress_O0_32x16_avx2;
+#endif
+#if defined(HAVE_SSE4_1) && defined(HAVE_SSSE3)
+        if (have_sse4_1)
+            return rans_compress_O0_32x16;
+#endif
+        return rans_compress_O0_32x16;
     }
-
-    // Else all SIMD disabled
-    return order & 1
-	? rans_compress_O1_4x16
-	: rans_compress_O0_4x16;
 }
 
 static inline
@@ -906,7 +916,11 @@ unsigned char *(*rans_dec_func(int do_simd, int order))
      unsigned char *out,
      unsigned int out_size) {
 
-    if (do_simd) {
+    if (!do_simd) { // SIMD disabled
+        return order & 1
+            ? rans_uncompress_O1_4x16
+            : rans_uncompress_O0_4x16;
+    }
 	unsigned int eax = 0, ebx = 0, ecx = 0, edx = 0;
 	int have_ssse3   = 0;
 	int have_sse4_1  = 0;
@@ -948,28 +962,34 @@ unsigned char *(*rans_dec_func(int do_simd, int order))
 //		have_ssse3, have_sse4_1, have_popcnt, have_avx2, have_avx512f);
 
 	if (order & 1) {
-	    return have_avx512f
-		? rans_uncompress_O1_32x16_avx512
-		: (have_avx2
-		   ? rans_uncompress_O1_32x16_avx2
-		   : (have_sse4_1
-		      ? rans_uncompress_O1_32x16_sse4
-		      : rans_uncompress_O1_32x16));
+#if defined(HAVE_AVX512)
+	    if (have_avx512f)
+            return rans_uncompress_O1_32x16_avx512;
+#endif
+#if defined(HAVE_AVX2)
+		if (have_avx2)
+		   return rans_uncompress_O1_32x16_avx2;
+#endif
+#if defined(HAVE_SSE4_1) && defined(HAVE_SSSE3)
+        if (have_sse4_1)
+            return rans_uncompress_O1_32x16_sse4;
+#endif
+        return rans_uncompress_O1_32x16;
 	} else {
-	    return have_avx512f
-		? rans_uncompress_O0_32x16_avx512
-		: (have_avx2
-		   ? rans_uncompress_O0_32x16_avx2
-		   : (have_sse4_1
-		      ? rans_uncompress_O0_32x16_sse4
-		      : rans_uncompress_O0_32x16));
+#if defined(HAVE_AVX512)
+	    if (have_avx512f)
+            return rans_uncompress_O0_32x16_avx512;
+#endif
+#if defined(HAVE_AVX2)
+		if (have_avx2)
+            return rans_uncompress_O0_32x16_avx2;
+#endif
+#if defined(HAVE_SSE4_1) && defined(HAVE_SSSE3)
+        if (have_sse4_1)
+            return rans_uncompress_O0_32x16_sse4;
+#endif
+        return rans_uncompress_O0_32x16;
 	}
-    }
-
-    // Else all SIMD disabled
-    return order & 1
-	? rans_uncompress_O1_4x16
-	: rans_uncompress_O0_4x16;
 }
 
 #elif defined(__ARM_NEON)

commit 34829bdae008cb20caf2df86fda852529fa6ec20

Fix broken indentation introduced in c8dd53c

htscodecs/rANS_static4x16pr.c
@@ -841,44 +841,44 @@ unsigned char *(*rans_enc_func(int do_simd, int order))
             ? rans_compress_O1_4x16
             : rans_compress_O0_4x16;
     }
-	unsigned int eax = 0, ebx = 0, ecx = 0, edx = 0;
-	int have_ssse3   = 0;
-	int have_sse4_1  = 0;
-	int have_popcnt  = 0;
-	int have_avx2    = 0;
-	int have_avx512f = 0;
+    unsigned int eax = 0, ebx = 0, ecx = 0, edx = 0;
+    int have_ssse3   = 0;
+    int have_sse4_1  = 0;
+    int have_popcnt  = 0;
+    int have_avx2    = 0;
+    int have_avx512f = 0;
 
-	int level = __get_cpuid_max(0, NULL);
-	if (level >= 1) {
-	    __cpuid_count(1, 0, eax, ebx, ecx, edx);
+    int level = __get_cpuid_max(0, NULL);
+    if (level >= 1) {
+	__cpuid_count(1, 0, eax, ebx, ecx, edx);
 #if defined(bit_SSSE3)
-	    have_ssse3 = ecx & bit_SSSE3;
+	have_ssse3 = ecx & bit_SSSE3;
 #endif
 #if defined(bit_POPCNT)
-	    have_popcnt = ecx & bit_POPCNT;
+	have_popcnt = ecx & bit_POPCNT;
 #endif
 #if defined(bit_SSE4_1)
-	    have_sse4_1 = ecx & bit_SSE4_1;
+	have_sse4_1 = ecx & bit_SSE4_1;
 #endif
-	}
-	if (level >= 7) {
-	    __cpuid_count(7, 0, eax, ebx, ecx, edx);
+    }
+    if (level >= 7) {
+	__cpuid_count(7, 0, eax, ebx, ecx, edx);
 #if defined(bit_AVX2)
-	    have_avx2 = ebx & bit_AVX2;
+	have_avx2 = ebx & bit_AVX2;
 #endif
 #if defined(bit_AVX512F)
-	    have_avx512f = ebx & bit_AVX512F;
+	have_avx512f = ebx & bit_AVX512F;
 #endif
-	}
+    }
 
-	if (!have_popcnt) have_avx512f = have_avx2 = have_sse4_1 = 0;
-	if (!have_ssse3)  have_sse4_1 = 0;
+    if (!have_popcnt) have_avx512f = have_avx2 = have_sse4_1 = 0;
+    if (!have_ssse3)  have_sse4_1 = 0;
 
-	if (!(rans_cpu & RANS_CPU_ENC_AVX512)) have_avx512f = 0;
-	if (!(rans_cpu & RANS_CPU_ENC_AVX2))   have_avx2 = 0;
-	if (!(rans_cpu & RANS_CPU_ENC_SSE4))   have_sse4_1 = 0;
+    if (!(rans_cpu & RANS_CPU_ENC_AVX512)) have_avx512f = 0;
+    if (!(rans_cpu & RANS_CPU_ENC_AVX2))   have_avx2 = 0;
+    if (!(rans_cpu & RANS_CPU_ENC_SSE4))   have_sse4_1 = 0;
 
-	if (order & 1) {
+    if (order & 1) {
 #if defined(HAVE_AVX512)
         if (have_avx512f)
             return rans_compress_O1_32x16_avx512;
@@ -892,20 +892,20 @@ unsigned char *(*rans_enc_func(int do_simd, int order))
             return rans_compress_O1_32x16;
 #endif
         return rans_compress_O1_32x16;
-	} else {
+    } else {
 #if defined(HAVE_AVX512)
-	    if (have_avx512f)
-            return rans_compress_O0_32x16_avx512;
+	if (have_avx512f)
+	    return rans_compress_O0_32x16_avx512;
 #endif
 #if defined(HAVE_AVX2)
-		if (have_avx2)
-            return rans_compress_O0_32x16_avx2;
+	if (have_avx2)
+	    return rans_compress_O0_32x16_avx2;
 #endif
 #if defined(HAVE_SSE4_1) && defined(HAVE_SSSE3)
-        if (have_sse4_1)
-            return rans_compress_O0_32x16;
+	if (have_sse4_1)
+	    return rans_compress_O0_32x16;
 #endif
-        return rans_compress_O0_32x16;
+	return rans_compress_O0_32x16;
     }
 }
 
@@ -921,75 +921,75 @@ unsigned char *(*rans_dec_func(int do_simd, int order))
             ? rans_uncompress_O1_4x16
             : rans_uncompress_O0_4x16;
     }
-	unsigned int eax = 0, ebx = 0, ecx = 0, edx = 0;
-	int have_ssse3   = 0;
-	int have_sse4_1  = 0;
-	int have_popcnt  = 0;
-	int have_avx2    = 0;
-	int have_avx512f = 0;
+    unsigned int eax = 0, ebx = 0, ecx = 0, edx = 0;
+    int have_ssse3   = 0;
+    int have_sse4_1  = 0;
+    int have_popcnt  = 0;
+    int have_avx2    = 0;
+    int have_avx512f = 0;
 
-	int level = __get_cpuid_max(0, NULL);
-	if (level >= 1) {
-	    __cpuid_count(1, 0, eax, ebx, ecx, edx);
+    int level = __get_cpuid_max(0, NULL);
+    if (level >= 1) {
+	__cpuid_count(1, 0, eax, ebx, ecx, edx);
 #if defined(bit_SSSE3)
-	    have_ssse3 = ecx & bit_SSSE3;
+	have_ssse3 = ecx & bit_SSSE3;
 #endif
 #if defined(bit_POPCNT)
-	    have_popcnt = ecx & bit_POPCNT;
+	have_popcnt = ecx & bit_POPCNT;
 #endif
 #if defined(bit_SSE4_1)
-	    have_sse4_1 = ecx & bit_SSE4_1;
+	have_sse4_1 = ecx & bit_SSE4_1;
 #endif
-	}
-	if (level >= 7) {
-	    __cpuid_count(7, 0, eax, ebx, ecx, edx);
+    }
+    if (level >= 7) {
+	__cpuid_count(7, 0, eax, ebx, ecx, edx);
 #if defined(bit_AVX2)
-	    have_avx2 = ebx & bit_AVX2;
+	have_avx2 = ebx & bit_AVX2;
 #endif
 #if defined(bit_AVX512F)
-	    have_avx512f = ebx & bit_AVX512F;
+	have_avx512f = ebx & bit_AVX512F;
 #endif
-	}
+    }
 
-	if (!have_popcnt) have_avx512f = have_avx2 = have_sse4_1 = 0;
-	if (!have_ssse3)  have_sse4_1 = 0;
+    if (!have_popcnt) have_avx512f = have_avx2 = have_sse4_1 = 0;
+    if (!have_ssse3)  have_sse4_1 = 0;
 
-	if (!(rans_cpu & RANS_CPU_DEC_AVX512)) have_avx512f = 0;
-	if (!(rans_cpu & RANS_CPU_DEC_AVX2))   have_avx2 = 0;
-	if (!(rans_cpu & RANS_CPU_DEC_SSE4))   have_sse4_1 = 0;
+    if (!(rans_cpu & RANS_CPU_DEC_AVX512)) have_avx512f = 0;
+    if (!(rans_cpu & RANS_CPU_DEC_AVX2))   have_avx2 = 0;
+    if (!(rans_cpu & RANS_CPU_DEC_SSE4))   have_sse4_1 = 0;
 
-//	fprintf(stderr, "SSSE3 %d, SSE4.1 %d, POPCNT %d, AVX2 %d, AVX512F %d\n",
-//		have_ssse3, have_sse4_1, have_popcnt, have_avx2, have_avx512f);
+    //	fprintf(stderr, "SSSE3 %d, SSE4.1 %d, POPCNT %d, AVX2 %d, AVX512F %d\n",
+    //		have_ssse3, have_sse4_1, have_popcnt, have_avx2, have_avx512f);
 
-	if (order & 1) {
+    if (order & 1) {
 #if defined(HAVE_AVX512)
-	    if (have_avx512f)
-            return rans_uncompress_O1_32x16_avx512;
+	if (have_avx512f)
+	    return rans_uncompress_O1_32x16_avx512;
 #endif
 #if defined(HAVE_AVX2)
-		if (have_avx2)
-		   return rans_uncompress_O1_32x16_avx2;
+	if (have_avx2)
+	    return rans_uncompress_O1_32x16_avx2;
 #endif
 #if defined(HAVE_SSE4_1) && defined(HAVE_SSSE3)
         if (have_sse4_1)
             return rans_uncompress_O1_32x16_sse4;
 #endif
         return rans_uncompress_O1_32x16;
-	} else {
+    } else {
 #if defined(HAVE_AVX512)
-	    if (have_avx512f)
-            return rans_uncompress_O0_32x16_avx512;
+	if (have_avx512f)
+	    return rans_uncompress_O0_32x16_avx512;
 #endif
 #if defined(HAVE_AVX2)
-		if (have_avx2)
-            return rans_uncompress_O0_32x16_avx2;
+	if (have_avx2)
+	    return rans_uncompress_O0_32x16_avx2;
 #endif
 #if defined(HAVE_SSE4_1) && defined(HAVE_SSSE3)
-        if (have_sse4_1)
-            return rans_uncompress_O0_32x16_sse4;
+	if (have_sse4_1)
+	    return rans_uncompress_O0_32x16_sse4;
 #endif
-        return rans_uncompress_O0_32x16;
-	}
+	return rans_uncompress_O0_32x16;
+    }
 }
 
 #elif defined(__ARM_NEON)