479b5be2fec9584ed3e1faf70c1527468d5f44bb
James Bonfield <jkb@sanger.ac.uk>
2023-04-18 15:17:01 +0100

    Only use the ARM NEON 32-way unrolled rANS on AArch64.
    NEON alone isn't a sufficient guard as AArch32 also has some limited
    Neon capabilities.  While we could no doubt have a 32-bit alternative,
    for now this is the simple fix and let aarch32 use the scalar
    implementation.
    
    Doing a 32-bit neon is a complex task and without having access to the
    hardware it's pretty much impossible.  I also wouldn't have high hopes
    for any significant speed gains over scalar with only half the lanes
    available.
    
    Fixes #81

--- htscodecs/rANS_static32x16pr_neon.c
+++ htscodecs/rANS_static32x16pr_neon.c
@@ -32,7 +32,7 @@
  */
 
 #include "config.h"
-#ifdef __ARM_NEON
+#if defined(__ARM_NEON) && defined(__aarch64__)
 #include <arm_neon.h>
 
 #include <limits.h>
--- htscodecs/rANS_static4x16pr.c
+++ htscodecs/rANS_static4x16pr.c
@@ -1006,7 +1006,7 @@ unsigned char *(*rans_dec_func(int do_simd, int order))
     }
 }
 
-#elif defined(__ARM_NEON)
+#elif defined(__ARM_NEON) && defined(__aarch64__)
 
 #if defined(__linux__) || defined(__FreeBSD__)
 #include <sys/auxv.h>
