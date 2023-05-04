938d0b5e0489ea97a1de771bf97ec2ef37d498b8
James Bonfield <jkb@sanger.ac.uk>
2023-05-04 15:17:10 +0100

    Add include of config.h to test programs.
    This helps pick up things like XOPEN_SOURCE definitions as required.

--- tests/entropy.c
+++ tests/entropy.c
@@ -31,6 +31,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+#include "config.h"
 
 /*
  * This test aims to test all entropy codecs on an input file.
